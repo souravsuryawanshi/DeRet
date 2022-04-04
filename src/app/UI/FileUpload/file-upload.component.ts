8432d53fbecf2508fc23d12009a10f16f66e2c96
Sourav Suryawanshi <ssuryawanshi1028@gmail.com>
2022-04-04 14:35:28 +0530

    later

--- src/app/UI/FileUpload/file-upload.component.ts
+++ src/app/UI/FileUpload/file-upload.component.ts
@@ -41,14 +41,16 @@ export class FileUploadComponent implements OnInit {
   async ngOnInit() {
     this.model = await tf.loadLayersModel('../../../assets/Model2/model.json');
     //model loaded
+    console.log('loading model');
     console.log(this.model);
-    this.fer_json = await $.getJSON(
-      '../../../assets/Model/fer.json',
-      function (json) {
-        return json.responseJSON;
-      }
-    );
-    console.log(this.fer_json);
+    console.log('sourav');
+    // this.fer_json = await $.getJSON(
+    //   '../../../assets/Model/fer.json',
+    //   function (json) {
+    //     return json.responseJSON;
+    //   }
+    // );
+    // console.log(this.fer_json);
   }
 
   // reader: any;
@@ -83,119 +85,115 @@ export class FileUploadComponent implements OnInit {
     img.src = this.url;
     let axis;
 
-    setTimeout(() => {
-      img.onload = async () => {
-        const t = await tf.browser.fromPixels(img);
-        console.log(t.shape);
-        let res = await tf.expandDims(t, (axis = 0));
+    img.onload = () => {
+      const t = tf.browser.fromPixels(img);
+      console.log(t.shape);
+      let res = tf.expandDims(t, (axis = 0));
 
-        //check
-        let tensor = await tf.browser.fromPixels(img);
+      //check
+      let tensor = tf.browser.fromPixels(img);
 
-        const resized = await tf.image
-          .resizeBilinear(tensor, [320, 320])
-          .toFloat();
+      const resized = tf.image.resizeBilinear(tensor, [320, 320]).toFloat();
 
-        const offset = await tf.scalar(255.0);
+      const offset = tf.scalar(255.0);
 
-        // working for normal but might not working good for eye
-        const normalized = await tf.scalar(1.0).sub(resized.div(offset)); // [ 0.0028980933129787445, 0.0005009372835047543, 0.9820129871368408, 0.005302976816892624, 0.009285111911594868 ]
-        //not working for normal but working good for eye
-        // const normalized = resized.div(offset); //[ 0.9991870522499084, 0.00011081025149906054, 0.00041361741023138165, 0.00005470188625622541, 0.0002339934289921075 ]
+      // working for normal but might not working good for eye
+      const normalized = tf.scalar(1.0).sub(resized.div(offset)); // [ 0.0028980933129787445, 0.0005009372835047543, 0.9820129871368408, 0.005302976816892624, 0.009285111911594868 ]
+      //not working for normal but working good for eye
+      // const normalized = resized.div(offset); //[ 0.9991870522499084, 0.00011081025149906054, 0.00041361741023138165, 0.00005470188625622541, 0.0002339934289921075 ]
 
-        const batched = await normalized.expandDims(0);
-        var pred = await this.model.predict(batched).dataSync();
-        console.log(pred);
+      const batched = normalized.expandDims(0);
+      var pred = this.model.predict(batched).dataSync();
+      console.log(pred);
 
-        //class detection from here
-        let count = 0;
-        let max;
-        for (let i = 0; i < 5; i++) {
-          if (pred[i] > count) {
-            count = pred[i];
-            max = i;
-          }
+      //class detection from here
+      let count = 0;
+      let max;
+      for (let i = 0; i < 5; i++) {
+        if (pred[i] > count) {
+          count = pred[i];
+          max = i;
         }
+      }
 
-        this.loading = !this.loading;
-        this.predicted = true;
-        switch (max) {
-          case 0:
-            console.log('class 1');
-            this.predictedClass = 'Class 1';
-            this.predictedClassMessage = 'No apparent Retinopathy is detected.';
-            this.severity = 'None';
-            this.consultation = 'No';
+      this.loading = !this.loading;
+      this.predicted = true;
+      switch (max) {
+        case 0:
+          console.log('class 1');
+          this.predictedClass = 'Class 1';
+          this.predictedClassMessage = 'No apparent Retinopathy is detected.';
+          this.severity = 'None';
+          this.consultation = 'No';
 
-            break;
-          case 1:
-            console.log('class 2');
-            this.predictedClass = 'Class 2';
-            this.predictedClassMessage = '';
-            this.severity = '';
-            break;
-          case 2:
-            console.log('class 3');
-            this.predictedClass = 'Class 3';
-            this.predictedClassMessage = '';
-            this.severity = '';
-            break;
-          case 3:
-            console.log('class 4');
-            this.predictedClass = 'Class 4';
-            this.predictedClassMessage = '';
-            this.severity = '';
-            break;
-          case 4:
-            console.log('class 5');
-            this.predictedClass = 'Class 5';
-            this.predictedClassMessage = '';
-            this.severity = '';
-            break;
-        }
+          break;
+        case 1:
+          console.log('class 2');
+          this.predictedClass = 'Class 2';
+          this.predictedClassMessage = '';
+          this.severity = '';
+          break;
+        case 2:
+          console.log('class 3');
+          this.predictedClass = 'Class 3';
+          this.predictedClassMessage = '';
+          this.severity = '';
+          break;
+        case 3:
+          console.log('class 4');
+          this.predictedClass = 'Class 4';
+          this.predictedClassMessage = '';
+          this.severity = '';
+          break;
+        case 4:
+          console.log('class 5');
+          this.predictedClass = 'Class 5';
+          this.predictedClassMessage = '';
+          this.severity = '';
+          break;
+      }
 
-        //class detection till here
+      //class detection till here
 
-        console.log('argmax');
-        const idx = tf.argMax(pred);
+      console.log('argmax');
+      const idx = tf.argMax(pred);
 
-        // var indices = findIndicesOfMax(pred, 1);
-        // console.log(indices);
-        // var probs = findTopValues(pred, 1);
-        // var names = getClassNames(indices);
-        console.log('argmax    ' + idx);
+      // var indices = findIndicesOfMax(pred, 1);
+      // console.log(indices);
+      // var probs = findTopValues(pred, 1);
+      // var names = getClassNames(indices);
+      console.log('argmax    ' + idx);
 
-        //checkend
+      //checkend
 
-        // console.log(this.model);
-        // console.log(this.model.predict(batched));
-      };
+      // console.log(this.model);
+      // console.log(this.model.predict(batched));
+    };
 
-      // console.log(this.reader);
-      // console.log(this.file);
-      // let res = tf.Tensor()
-      // console.log(res);
+    // console.log(this.reader);
+    // console.log(this.file);
+    // let res = tf.Tensor()
+    // console.log(res);
 
-      // const image = new Image();
-      // image.src = this.reader;
-      // image.onload = () => {
-      //   const a = tf.FromPixels(image, 320);
-      //   a.print();
-      //   console.log(a.shape);
-      // };
-      // let res = tf.image.resizeBilinear(this.file, [320, 320]);
-      // console.log('result' + res);
-      // setTimeout(() => {
-      //   console.log('class not existing');
-      // }, 10000);
-      // this.fileUploadService.upload(this.file).subscribe((event: any) => {
-      //   if (typeof event === 'object') {
-      //     // Short link via api response
-      //     this.shortLink = event.link;
+    // const image = new Image();
+    // image.src = this.reader;
+    // image.onload = () => {
+    //   const a = tf.FromPixels(image, 320);
+    //   a.print();
+    //   console.log(a.shape);
+    // };
+    // let res = tf.image.resizeBilinear(this.file, [320, 320]);
+    // console.log('result' + res);
+    // setTimeout(() => {
+    //   console.log('class not existing');
+    // }, 10000);
+    // this.fileUploadService.upload(this.file).subscribe((event: any) => {
+    //   if (typeof event === 'object') {
+    //     // Short link via api response
+    //     this.shortLink = event.link;
 
-      //     this.loading = false; // Flag variable
-      //   }
-      // });
-    }, 10000);
+    //     this.loading = false; // Flag variable
+    //   }
+    // });
   }
 }
